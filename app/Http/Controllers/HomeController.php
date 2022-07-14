56168da12206af9202b04079379da00db99257cf
Md.Yeasin Hossain Imran <63287530+Yeasin123@users.noreply.github.com>
2022-07-14 14:38:10 +0600

    laravel fcm

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,80 @@
+<?php
+  
+namespace App\Http\Controllers;
+  
+use Illuminate\Http\Request;
+use App\Models\User;
+  
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+  
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+  
+    /** 
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function saveToken(Request $request)
+    {
+        auth()->user()->update(['device_token'=>$request->imran]);
+        return response()->json(['token saved successfully.']);
+    }
+  
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function sendNotification(Request $request)
+    {
+        // $this->saveToken($request);
+        $firebaseToken = User::whereNotNull('device_token')->pluck('device_token')->all();
+          
+        $SERVER_API_KEY = 'AAAANR2ZBpI:APA91bGUE41VhfEbiz_TfdfFrEWd1lRtx1QpK4ZIy8R1wgubnSbuHVUoABVs6kaFQFn3yTw_vbxrVdbDu8Fdrasbzj0MVdq5qmtzIpyvbQcZJ6Zs4ZqWHeCudDVbMaptLCiTFhkLktYg';
+  
+        $data = [
+            "registration_ids" => $firebaseToken,
+            "notification" => [
+                "title" => $request->title,
+                "body" => $request->body,  
+            ]
+        ];
+        $dataString = json_encode($data);
+    
+        $headers = [
+            'Authorization: key=' . $SERVER_API_KEY,
+            'Content-Type: application/json',
+        ];
+    
+        $ch = curl_init();
+      
+        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+               
+        $response = curl_exec($ch);
+  
+       return $response;
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.app')
+   
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <center>
+                <button id="btn-nft-enable" onclick="initFirebaseMessagingRegistration()" class="btn btn-danger btn-xs btn-flat">Allow for Notification</button>
+            </center>
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+  
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+  
+                    <form action="{{ route('send.notification') }}" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <label>Title</label>
+                            <input type="text" class="form-control" name="title">
+                        </div>
+                        <div class="form-group">
+                            <label>Body</label>
+                            <textarea class="form-control" name="body"></textarea>
+                          </div>
+                        <button type="submit" class="btn btn-primary">Send Notification</button>
+                    </form>
+  
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.23.0/firebase.js"></script>
+<script>
+  
+    var firebaseConfig = {
+        apiKey: "AIzaSyAUON1WPvBlI8zs8nhgSTE7SplCSXCUHtE",
+        authDomain: "fcmlaravel-feed2.firebaseapp.com",
+        projectId: "fcmlaravel-feed2",
+        storageBucket: "fcmlaravel-feed2.appspot.com",
+        messagingSenderId: "228129834642",
+        appId: "1:228129834642:web:02493a83ab93254608484b",
+        measurementId: "G-XSFSERWSQV"
+    };
+      
+    firebase.initializeApp(firebaseConfig);
+    const messaging = firebase.messaging();
+  
+    function initFirebaseMessagingRegistration() {
+            messaging
+            .requestPermission()
+            .then(function () {
+                return messaging.getToken()
+            })
+            .then(function(token) {
+                console.log(token);
+   
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+  
+                $.ajax({
+                    url: '{{ route("save-token") }}',
+                    type: 'POST',
+                    data: {
+                        imran: token,
+                        _token: "{{ csrf_token() }}",
+                    },
+                    dataType: 'JSON',
+                    success: function (response) {
+                        alert('Token saved successfully.');
+                    },
+                    error: function (err) {
+                        console.log('User Chat Token Error'+ err);
+                    },
+                });
+  
+            }).catch(function (err) {
+                console.log('User Chat Token Error'+ err);
+            });
+     }  
+      
+    messaging.onMessage(function(payload) {
+        const noteTitle = payload.notification.title;
+        const noteOptions = {
+            body: payload.notification.body,
+            icon: payload.notification.icon,
+        };
+        new Notification(noteTitle, noteOptions);
+    });
+   
+</script>
+@endsection
